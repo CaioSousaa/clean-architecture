c207715a56b015e0e3340183f892d62f269c3bfe
caio <caiothalles607@gmail.com>
2024-09-03 08:26:40 -0300

    feat: initial commit

---
+++ src/main/config/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { initializationError } from "../middleware/initialization-error-";
+
+const app = express();
+
+app.use(express.json());
+app.use(initializationError);
+
+export { app };
---
+++ src/main/server.ts
@@ -0,0 +1,3 @@
+import { app } from "./config/app";
+
+app.listen(process.env.PORT || 3000, () => console.log("server is run"));
