a230f32599b892d89f9fd78562ba6cb92e8860bc
caio <caiothalles607@gmail.com>
2024-09-03 21:06:36 -0300

    feat: created data-source

---
+++ src/external/postgres/data-source.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../../entities/user/user";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.log("Error during Data Source initialization:", err);
+  });
