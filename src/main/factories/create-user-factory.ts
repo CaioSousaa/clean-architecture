51924e40d8fa56992459aee2875249e8b2b9f742
caio <caiothalles607@gmail.com>
2024-09-05 20:53:52 -0300

    feat: create user factory

---
+++ src/main/factories/create-user-factory.ts
@@ -0,0 +1,11 @@
+import { TypeOrmUserRepository } from "../../external/repositories/user-repository/typeOrm-user-repository";
+import { CreateUser } from "../../usecases/create-user/create-user";
+import { CreateUserController } from "../../adapters/presentation/controllers/user-controller/create-user-controller";
+
+export const createUserFactory = () => {
+  const typeOrmUserRepository = new TypeOrmUserRepository();
+  const createUser = new CreateUser(typeOrmUserRepository);
+  const createUserController = new CreateUserController(createUser);
+
+  return createUserController;
+};
