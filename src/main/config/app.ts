683753df00ee1e2579d364faf8d5dd77c30a02e1
caio <caiothalles607@gmail.com>
2024-09-05 20:54:26 -0300

    chore: add use userRoutes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
 import { initializationError } from "../middleware/initialization-error-";
+import { userRoutes } from "../routes/user.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(initializationError);
+app.use(userRoutes);
 
 export { app };
