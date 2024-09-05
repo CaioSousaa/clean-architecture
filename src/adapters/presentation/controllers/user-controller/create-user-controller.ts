8398f9c2f30394fa0d2c28bb0efc3e4eb67c0e3e
caio <caiothalles607@gmail.com>
2024-09-05 20:53:37 -0300

    feat: create user controller

---
+++ src/adapters/presentation/controllers/user-controller/create-user-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateUser } from "../../../../usecases/create-user/create-user";
+
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUser) {}
+  async handle(req: Request, res: Response) {
+    const { first_name, surname, age, address, unique_identifier } = req.body;
+
+    const user = await this.createUserUseCase.execute({
+      first_name,
+      surname,
+      address,
+      age,
+      unique_identifier,
+    });
+
+    res.status(201).json(user);
+  }
+}
