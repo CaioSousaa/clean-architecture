6fdef4ec4b512ebaa493425aead69a0d9bba0d0e
caio <caiothalles607@gmail.com>
2024-09-05 21:24:15 -0300

    chore: add get route

--- src/main/routes/user.routes.ts
+++ src/main/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createUserFactory } from "../factories/create-user-factory";
+import { getAllUsersFactory } from "../factories/get-all-users-factory";
 
 const userRoutes = Router();
 
 userRoutes.post("/user", async (req, res) => {
   return createUserFactory().handle(req, res);
+});
+
+userRoutes.get("/", async (req, res) => {
+  return getAllUsersFactory().handle(req, res);
 });
 
 export { userRoutes };
