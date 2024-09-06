4e7c84fb7cb84bacdc602753f271db6bfebb5ff1
caio <caiothalles607@gmail.com>
2024-09-06 18:47:01 -0300

    feat: add endpoint to search user by id

--- src/main/routes/user.routes.ts
+++ src/main/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUserFactory } from "../factories/create-user-factory";
 import { getAllUsersFactory } from "../factories/get-all-users-factory";
+import { findUserByIdFactory } from "../factories/find-by-id-factory";
 
 const userRoutes = Router();
 
@@ -10,6 +11,10 @@ userRoutes.post("/user", async (req, res) => {
 
 userRoutes.get("/", async (req, res) => {
   return getAllUsersFactory().handle(req, res);
+});
+
+userRoutes.get("/user/:id", async (req, res) => {
+  return findUserByIdFactory().handle(req, res);
 });
 
 export { userRoutes };
