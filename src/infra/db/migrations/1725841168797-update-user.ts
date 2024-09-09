18a63361be124d3fe9f5878bb116a95abe80dc8e
caio <caiothalles607@gmail.com>
2024-09-09 14:40:09 -0300

    fix : correcting bug generated due to lack of reference to the card entity

--- src/infra/db/migrations/1725841168797-update-user.ts
+++ src/infra/db/migrations/1725841168797-update-user.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from "typeorm";
 
 export class UpdateUser1725841168797 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -24,9 +29,20 @@ export class UpdateUser1725841168797 implements MigrationInterface {
         isNullable: true,
       }),
     ]);
+
+    await queryRunner.createForeignKey(
+      "user",
+      new TableForeignKey({
+        columnNames: ["card_bank_id"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "card_bank",
+        onDelete: "CASCADE",
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey("user", "FK_user_card_bank");
     await queryRunner.dropColumns("user", [
       "email",
       "password",
