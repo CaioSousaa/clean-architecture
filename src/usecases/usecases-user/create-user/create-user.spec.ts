808675d0172e1d5fcd4969f0a5b7635fdd8c1190
caio <caiothalles607@gmail.com>
2024-09-11 21:09:41 -0300

    feat: created unit tests for create-user

---
+++ src/usecases/usecases-user/create-user/create-user.spec.ts
@@ -0,0 +1,109 @@
+require("dotenv").config();
+import { User } from "../../../domain/entities/user";
+import { CreateUser } from "./create-user";
+import { IUserRepository } from "../../ports/IUser-respository";
+import { InMemoryUserRepository } from "../in-memory/in-memory-user-repository";
+
+describe("create-user", () => {
+  let inMemoryUserRepository: IUserRepository;
+  let createUserUseCase: CreateUser;
+
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    createUserUseCase = new CreateUser(inMemoryUserRepository);
+  });
+
+  it("it must be possible to create a user with all valid fields", async () => {
+    const userDate: User = {
+      first_name: "Joe",
+      surname: "Doe",
+      address: "Rua teste 123 Brasil",
+      age: 23,
+      card_bank: null,
+      created_at: new Date(),
+      status_plan: false,
+      email: "emailtest@gmail.com",
+      password: "Test!1",
+      unique_identifier: process.env.VALID_CPF,
+    };
+
+    const user = await createUserUseCase.execute(userDate);
+
+    expect(user).toHaveProperty("first_name");
+    expect(user).toHaveProperty("surname");
+    expect(user).toHaveProperty("age");
+    expect(user).toHaveProperty("status_plan");
+    expect(user).toHaveProperty("email");
+    expect(user).toHaveProperty("unique_identifier");
+  });
+
+  it("it should not be possible to create a user with a unique identifier that does not comply with", async () => {
+    const userDate: User = {
+      first_name: "Joe",
+      surname: "Doe",
+      address: "Rua teste 123 Brasil",
+      age: 23,
+      card_bank: null,
+      created_at: new Date(),
+      status_plan: false,
+      email: "emailtest@gmail.com",
+      password: "Test!1",
+      unique_identifier: "23087625590",
+    };
+
+    const response = await createUserUseCase.execute(userDate);
+
+    expect(response).toEqual({
+      description: "we're sorry, but the unique identifier is invalid",
+      statusCode: 400,
+    });
+  });
+
+  it("it should not be possible to create a user with a non-standard email", async () => {
+    const userDate: User = {
+      first_name: "Joe",
+      surname: "Doe",
+      address: "Rua teste 123 Brasil",
+      age: 23,
+      card_bank: null,
+      created_at: new Date(),
+      status_plan: false,
+      email: "emailtestgmail.com",
+      password: "Test!1",
+      unique_identifier: process.env.VALID_CPF,
+    };
+
+    const response = await createUserUseCase.execute(userDate);
+
+    expect(response).toEqual({
+      description: "the email must meet the format: xxxx@gmail.com",
+      statusCode: 400,
+    });
+  });
+
+  it("should not be possible to create a user with the same unique identifier", async () => {
+    const userDate: User = {
+      first_name: "Joe",
+      surname: "Doe",
+      address: "Rua teste 123 Brasil",
+      age: 23,
+      card_bank: null,
+      created_at: new Date(),
+      status_plan: false,
+      email: "emailtest@gmail.com",
+      password: "Test!1",
+      unique_identifier: process.env.VALID_CPF,
+    };
+
+    await createUserUseCase.execute(userDate);
+
+    const result = await createUserUseCase
+      .execute(userDate)
+      .catch((err) => err);
+
+    expect(result).toEqual({
+      description: "unique identifier is already being used by another user",
+      statusCode: 401,
+    });
+  });
+});
