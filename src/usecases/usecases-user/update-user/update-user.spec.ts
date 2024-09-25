9e6f11e5eff441b32a89baf4813238929d4822e1
caio <caiothalles607@gmail.com>
2024-09-25 14:04:22 -0300

    fix: fixing a bug and adding a new test

--- src/usecases/usecases-user/update-user/update-user.spec.ts
+++ src/usecases/usecases-user/update-user/update-user.spec.ts
@@ -47,7 +47,7 @@ describe("update user", () => {
       used: false,
       token:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjcwOTY3MTUsImV4cCI6MTcyNzE4MzExNSwic3ViIjoiNTkyZWRjY2YtZjgxMS00NzM2LWIwYjItMTNjNDZmOTQ3ZGQxIn0.h6tpxIhUd0KvNYocBRO_vGaK67mSm1SazvzvSQPP79g",
-      expires_in: 1727183115,
+      expires_in: 3227183115, //use valid expires in
       created_at: new Date(),
     });
 
@@ -61,5 +61,41 @@ describe("update user", () => {
 
     expect(userUpdate.email).toBe("girafa@gmail.com");
     expect(userUpdate.address).toBe("Savana Leste, rua pe de jambo");
+  });
+
+  test("it should not be possible to change a user with expires in expired", async () => {
+    const userData: User = {
+      first_name: "Joe",
+      surname: "Doe",
+      address: "Rua teste 123 Brasil",
+      age: 23,
+      card_bank: null,
+      created_at: new Date(),
+      status_plan: false,
+      email: "emailtest@gmail.com",
+      password: "Test!1",
+      unique_identifier: process.env.VALID_CPF,
+    };
+
+    const user = await createUser.execute(userData);
+
+    const token = await inMemoryTokenRepository.create({
+      user_id: user.id,
+      used: false,
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjcwOTY3MTUsImV4cCI6MTcyNzE4MzExNSwic3ViIjoiNTkyZWRjY2YtZjgxMS00NzM2LWIwYjItMTNjNDZmOTQ3ZGQxIn0.h6tpxIhUd0KvNYocBRO_vGaK67mSm1SazvzvSQPP79g",
+      expires_in: 1027183115,
+      created_at: new Date(),
+    });
+
+    const userUpdate = await updateUser.execute({
+      id: user.id,
+      token: token.token,
+      email: "girafa@gmail.com",
+      password: "Girafa123!",
+      address: "Savana Leste, rua pe de jambo",
+    });
+
+    expect(userUpdate.email).not.toBeDefined();
   });
 });
